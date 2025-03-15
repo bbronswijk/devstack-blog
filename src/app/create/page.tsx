2c565f8d0e32904c9368aabd9ad58cf4ab8aae1f
bbronswijk <bbronswijk@anwb.nl>
2025-03-15 14:41:17 +0100

    increase timeout

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,7 @@
 import { Form } from "@/app/create/form";
 
+export const maxDuration = 60; // This function can run for a maximum of 60 seconds
+
 export default function Page() {
   return <Form />;
 }
